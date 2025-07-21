d4c057673247d2944df0fc970d3752c3781b5f8a
SeungIlB <piseouk787@gmail.com>
2025-07-21 14:40:58 +0900

    Add: 유저 디테일 추가

---
+++ src/main/java/seungil/login_boilerplate/domain/CustomUserDetails.java
@@ -0,0 +1,47 @@
+package seungil.login_boilerplate.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final String userId;
+    private final String password;
+    private final boolean accountNonExpired; // 계정 만료 여부
+    private final boolean accountNonLocked; // 계정 잠김 여부
+    private final boolean credentialsNonExpired; // 자격 증명 만료 여부
+    private final boolean enabled; // 계정 활성화 여부
+    private final Collection<? extends GrantedAuthority> authorities; // 사용자 권한 목록
+
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
