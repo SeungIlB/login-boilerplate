1854906c000578ecb3362c06b8fd1cd29bb7af36
SeungIlB <piseouk787@gmail.com>
2025-07-21 14:38:26 +0900

    Add: 가입 시간 등 디테일한 내용 추가

--- src/main/java/seungil/login_boilerplate/domain/User.java
+++ src/main/java/seungil/login_boilerplate/domain/User.java
@@ -1,13 +1,13 @@
 package seungil.login_boilerplate.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -20,9 +20,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
 
+    @Column(nullable = false)
     private String userId;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String userName;
+
+    @CreationTimestamp // INSERT 쿼리가 발생할 때, 현재 시간을 자동으로 저장
+    private LocalDateTime created_at; // 회원가입한 시간
+
+    @UpdateTimestamp // UPDATE 쿼리가 발생할 때, 현재 시간을 자동으로 저장
+    private LocalDateTime updated_at; // 마지막으로 수정한 시간
+
+    private boolean accountNonExpired; // 계정 만료 여부
+    private boolean accountNonLocked; // 계정 잠김 여부
+    private boolean credentialsNonExpired; // 자격 증명 만료 여부
+    private boolean enabled; // 계정 활성화 여부
 }
