64e022537335c2d5adade3461a686dd84383e573
SeungIlB <piseouk787@gmail.com>
2025-07-21 16:17:26 +0900

    Add: 생성자 어노테이션 추가

--- src/main/java/seungil/login_boilerplate/dto/UserResponseDTO.java
+++ src/main/java/seungil/login_boilerplate/dto/UserResponseDTO.java
@@ -1,15 +1,13 @@
 package seungil.login_boilerplate.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class UserResponseDTO {
 
