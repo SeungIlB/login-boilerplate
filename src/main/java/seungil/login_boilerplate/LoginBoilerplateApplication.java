35e5a5f746c96bfeb018c0b82c4820d14b6369d6
SeungIlB <piseouk787@gmail.com>
2025-07-21 13:57:35 +0900

    Fix line endings

---
+++ src/main/java/seungil/login_boilerplate/LoginBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package seungil.login_boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seungil/login_boilerplate/LoginBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package seungil.login_boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
