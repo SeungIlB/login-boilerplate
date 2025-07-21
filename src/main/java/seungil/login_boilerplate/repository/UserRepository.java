2818237bf058692c9fe3728c3e64a1eead02214c
SeungIlB <piseouk787@gmail.com>
2025-07-21 15:53:11 +0900

    Feat: 유저존재 및 찾는 기능

--- src/main/java/seungil/login_boilerplate/repository/UserRepository.java
+++ src/main/java/seungil/login_boilerplate/repository/UserRepository.java
@@ -4,8 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import seungil.login_boilerplate.domain.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUserId(String userId);
+
+    boolean existsByUserId(String userId);
 }
