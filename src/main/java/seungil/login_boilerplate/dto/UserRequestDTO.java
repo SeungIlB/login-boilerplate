44aaa3d2cfe97c5f3edabb42269ed7c8ba4f7ca5
SeungIlB <piseouk787@gmail.com>
2025-07-21 15:53:43 +0900

    Add: 유저데이터 필드 추가

--- src/main/java/seungil/login_boilerplate/dto/UserRequestDTO.java
+++ src/main/java/seungil/login_boilerplate/dto/UserRequestDTO.java
@@ -1,10 +1,32 @@
 package seungil.login_boilerplate.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import jakarta.validation.constraints.Pattern;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class UserRequestDTO {
+
+    @NotBlank(message = "아이디를 입력해주세요")
+    private final String userId;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
+            message = "비밀번호는 영문 대소문자, 숫자, 특수문자를 포함하여 8~16자여야 합니다")
+    private final String password;
+
+    @NotBlank(message = "사용자 이름을 입력해주세요")
+    private final String username;
+
+    @Override
+    public String toString() {
+        return "SignUpRequest{" +
+                "username='" + username + '\'' +
+                ", password='[PROTECTED]'" +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
 }
--- src/main/java/seungil/login_boilerplate/dto/UserResponseDTO.java
+++ src/main/java/seungil/login_boilerplate/dto/UserResponseDTO.java
@@ -5,9 +5,15 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class UserResponseDTO {
+
+    private UUID id;
+    private String userId;
+    private String userName;
 }
