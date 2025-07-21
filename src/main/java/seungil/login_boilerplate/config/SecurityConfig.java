f27530adf82a78e5d51072e92b0839bdc21763c3
SeungIlB <piseouk787@gmail.com>
2025-07-21 17:45:05 +0900

    Feat: 로그인 기능 구현

--- src/main/java/seungil/login_boilerplate/config/SecurityConfig.java
+++ src/main/java/seungil/login_boilerplate/config/SecurityConfig.java
@@ -12,11 +12,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import seungil.login_boilerplate.jwt.JwtAuthenticationFilter;
+import seungil.login_boilerplate.jwt.JwtTokenProvider;
+import seungil.login_boilerplate.service.CustomUserDetailsService;
+
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean // 비밀번호 암호화를 위한 PasswordEncoder 빈 생성
     public PasswordEncoder passwordEncoder() {
@@ -28,20 +36,21 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Bean // SecurityFilterChain 설정 (스프링 부트 3부터는 FilterChain 방식 사용)
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                // REST API에서는 불필요한 CSRF 보안 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
-                // JWT 토큰 인증 시스템을 사용할 것이기에 서버가 세션을 생성하지 않도록 한다.
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // HTTP 요청에 대한 인가 규칙 설정
                 .authorizeHttpRequests(auth -> auth
-                        // 로그인과 회원가입 페이지는 누구나 접근 가능
-                        .requestMatchers("/login", "/signup").permitAll()
-                        .anyRequest().authenticated() // 그 외 요청은 인증 필요
-                );
-
+                        .requestMatchers("/login", "/auth/login", "/user/signup", "/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .permitAll() // 로그인 페이지는 누구나 접근 가능
+                )
+                // JWT 인증 필터 추가
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider,
+                        customUserDetailsService), UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 }
---
+++ src/main/java/seungil/login_boilerplate/service/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package seungil.login_boilerplate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import seungil.login_boilerplate.domain.CustomUserDetails;
+import seungil.login_boilerplate.domain.User;
+import seungil.login_boilerplate.repository.UserRepository;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override // userId을 기준으로 사용자를 로드하는 메서드
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        // userId로 사용자 조회
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 userId를 가진 사용자를 찾을 수 없습니다: " + userId));
+
+        // 조회된 사용자 정보를 기반으로 CustomUserDetails 객체 생성 후 반환
+        return new CustomUserDetails(
+                user.getUserName(),
+                user.getPassword(),
+                user.getUserId(),
+                user.isAccountNonExpired(), // 계정 만료 여부
+                user.isAccountNonLocked(), // 계정 잠김 여부
+                user.isCredentialsNonExpired(), // 자격 증명 만료 여부
+                user.isEnabled(), // 계정 활성화 여부
+                Collections.emptyList()
+        );
+
+    }
+}
